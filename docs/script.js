716d2b29e33ed8e8ce8d34d8bf2a8ba418abed62
sourkr <sourav.kumar.all.2021@gmail.com>
2024-05-12 17:01:22 +0530

    added code block

--- docs/script.js
+++ docs/script.js
@@ -1,7 +1,13 @@
+import { Interprater } from './src/sour-interprater/interprater.js';
+
 const tag = /<(?<tag>.*?)>(.*?)<\/\k<tag>>/g
 const version = '0.1 Beta'
 
 function lint(code) {
+  code.replace(/(\s+)$/, (f, s) => {
+    code = code.replace(RegExp(`^\\s{0,${s.length + 1}}`, 'gm'), '')
+  })
+  
   code = code
     .replaceAll(/(class|fun)\b/g, `<keydef>$1</keydef>`)
     .replaceAll(/(return|export|import|new)/g, `<key>$1</key>`)
@@ -51,3 +57,85 @@ function highlight() {
 function header() {
   document.querySelector('.subtitle').innerText = version
 }
+
+const template = (html`
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@40,400,1,0" />
+
+<style>
+  .root {
+    background: white;
+    border: 1px solid hsl(50 50% 50%);
+    border-radius: 10px;
+  }
+  
+  .root div {
+    display: flex;
+    padding: 10px;
+    border-bottom: 1px solid hsl(50 50% 50%);
+    align-items: center;
+    color: hsl(50, 50%, 30%);
+  }
+  
+  .root div span:first-child {
+    flex: 1;
+  }
+  
+  code {
+    background: red;
+  }
+  
+  pre {
+    margin: 0;
+    border-top: 1px solid hsl(50 50% 50%);
+    display: none;
+    padding: 10px;
+  }
+  
+  #run {
+    color: hsl(50, 100%, 30%);
+  }
+</style>
+
+<div class="root">
+  <div>
+    <span><slot name="name"></slot></span>
+    <span id="run" class="material-symbols-rounded">play_arrow</span>
+  </div>
+  <slot id="code" name="code"></slot>
+  <pre id="out"></pre>
+</div>
+`)
+
+function html(a, ...b) {
+  const code = a
+    .map((a, i) => i==0?a:b[i-1]+a)
+    .join('')
+    
+  const template = document.createElement('template')
+  template.innerHTML = code
+  return template.content
+}
+
+class CodeBlock extends HTMLElement {
+  constructor() {
+    super()
+    
+    const root = this.attachShadow({ mode: 'open' })
+    root.appendChild(template.cloneNode(true))
+    
+    root.getElementById('run').onclick = () => {
+      const code = root.getElementById('code').assignedElements()[0].innerText
+      const out = root.getElementById('out') //.assignedElements()[0].value
+      
+      out.style.display = 'block'
+      
+      const interprater = new Interprater(msg => {
+        out.innerText += msg
+      })
+      
+      interprater.interprateCode(code)
+    }
+  }
+}
+
+customElements.define('code-block', CodeBlock)
