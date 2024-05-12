abc5beb1e4bbdc8126e43436ada9793e5e0ae6dc
sourkr <sourav.kumar.all.2021@gmail.com>
2024-05-12 12:06:18 +0530

    update

--- script.js
+++ script.js
@@ -1,42 +1,39 @@
-function highlightSour(code) {
+function lint(code) {
   code = code
-    .replaceAll(/(class|fun)\b/g, `<key-def>$1</key-def>`)
+    .replaceAll(/(class|fun)\b/g, `<keydef>$1</keydef>`)
     .replaceAll(/(return|export|import)/g, `<key>$1</key>`)
     .replace(/\b(\d+)/gm, `<num>$1</num>`)
     .replace(/(true|false)/gm, `<bool>$1</bool>`)
     .replace(/(\w+)\(/g, `<fname>$1</fname>(`)
     .replace(/([(+){=}])/g, `<punc>$1</punc>`)
     .replace(/\b([A-Z]\w+)/g, `<cls>$1</cls>`)
-    .replace(/(var) (\w+)/g, `<key-def>$1</key-def> <vname>$2</vname>`)
+    .replace(/(var) (\w+)/g, `<keydef>$1</keydef> <vname>$2</vname>`)
     
   code = code
-    .replace(/\/\/.*$/gm, full => {
-      full = full
-        .replace(/\<.*?>|<\\\w+>/g, '')
-        
-      return `<cmt>${full}</cmt>`
-    })
-    
-  code = code
-    .replace(/".*?"/g, full => {
+    .replace(/".*?(?<!\\)"/g, full => {
       full = full
         .replace(/\<\w+>|<\\\w+>/g, '')
       
       return `<str>${full}</str>`
     })
     
+  code = code
+    .replace(/\/\/.*$/gm, full => {
+      full = full
+        .replace(/\<.*?>|<\\\w+>/g, '')
+  
+      return `<cmt>${full}</cmt>`
+    })
+    
   return code
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-  highlightSourCode();
-});
-
-function highlightSourCode() {
-  const codeElements = document.querySelectorAll("pre.code");
-  for (const element of codeElements) {
-    const code = element.textContent;
-    const highlightedHTML = highlightSour(code);
-    element.innerHTML = highlightedHTML;
+  const pres = document.querySelectorAll("code pre");
+  
+  for (const pre of pres) {
+    const code = pre.innerText;
+    const html = lint(code);
+    pre.innerHTML = html;
   }
-}
+});
