a44ef0fbcbf1585f98b8b80e36d8b7b4146fa095
sourkr <sourav.kumar.all.2021@gmail.com>
2024-05-18 07:59:34 +0530

    update playground

--- docs/playground/script.js
+++ docs/playground/script.js
@@ -4,12 +4,22 @@ const code = document.getElementById('code')
 const run = document.getElementById('run')
 const output = document.getElementById('output')
 
-run.onclick = () => {
+code.onfocus = () => {
+  output.style.display = 'none'
+}
+
+code.onblur = () => {
+  output.style.display = 'block'
+}
+
+run.onclick = async () => {
   output.innerText = ''
   
-  const interprater = new Interprater(msg => {
-    output.innerText += msg + '\n'
-  })
-  
+  const interprater = new Interprater()
   interprater.interprateCode(code.value)
+  
+  while(true) {
+    const msg = await interprater.stream.read()
+    output.innerText += msg + '\n'
+  }
 }
